6286a0a610f345a880b956a9c36d8a671e915e97
Eden Eltume <IEltume@gmail.com>
2022-04-24 18:33:13 -0400

    Homepage js
    add javascript video functionality to homepage gallery

---
+++ javascript/punctuallimo.js
@@ -0,0 +1,80 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("bachelor-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("bachelor-picture").style.display = "none";
+        document.getElementById("bachelor-video").style.display = "block";
+    }
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("birthday-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("birthday-picture").style.display = "none";
+        document.getElementById("birthday-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("corporate-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("corporate-picture").style.display = "none";
+        document.getElementById("corporate-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("night-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("night-picture").style.display = "none";
+        document.getElementById("night-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("prom-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("prom-picture").style.display = "none";
+        document.getElementById("prom-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("shopping-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("shopping-picture").style.display = "none";
+        document.getElementById("shopping-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("sport-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("sport-picture").style.display = "none";
+        document.getElementById("sport-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("wedding-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("wedding-picture").style.display = "none";
+        document.getElementById("wedding-video").style.display = "block";
+    }
+})
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("wine-picture").addEventListener('click', displayVideo);
+
+    function displayVideo(){
+        document.getElementById("wine-picture").style.display = "none";
+        document.getElementById("wine-video").style.display = "block";
+    }
+})
